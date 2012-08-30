bffee24e4279c308282d62bf30fc8560cc54e350
Romaric Drigon <romaric.drigon@pmsipilot.com>
2012-08-30 17:06:07 +0200

    Added tests for loaders

---
+++ test/unit/RomaricDrigon/MetaYaml/Loader/JsonLoader.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace test\unit\RomaricDrigon\MetaYaml\Loader;
+
+use mageekguy\atoum;
+use RomaricDrigon\MetaYaml\Loader\JsonLoader as testedClass;
+
+class JsonLoader extends atoum\test
+{
+    public function testBase()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\JsonLoader')
+                ->array($object->loadFromFile('test/data/TestBasicBase.json'))
+                    ->isEqualTo(array(
+                        'fleurs' => array(
+                            'rose' => 'une rose',
+                            'violette' => 'une violette'
+                        )
+                    ));
+    }
+}
