438cea47bf45dcbe8f94ffdd2137ef9030e32fcb
Romaric Drigon <romaric.drigon@pmsipilot.com>
2012-08-31 15:36:16 +0200

    Added XmlLoader, first version

---
+++ src/RomaricDrigon/MetaYaml/Loader/XmlLoader.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace RomaricDrigon\MetaYaml\Loader;
+
+class XmlLoader extends Loader
+{
+    public function load($string, $ignore_first_node = false)
+    {
+        // we will parse XML using the convention (cf doc.)
+
+        // elements and attributes are stored in arrays
+        // multiple elements will give a prototype, using "_key" as name -> else nodes will be merged
+        // namespace are ditched
+        // empty nodes are suppressed
+        // node _value
+
+        if (! $ignore_first_node) {
+            $string = '<xml>'.$string.'</xml>';
+        }
+
+        $sxi = simplexml_load_string($string, 'SimpleXmlIterator');
+
+        if ($sxi === false) {
+            throw new \Exception('Error in XmlLoader : XML seems to be invalid');
+        }
+
+        return $this->xmlToArray($sxi);
+    }
+
+    public function loadFromFile($filename)
+    {
+        $xml = parent::loadFromFile($filename);
+
+        return $this->load($xml);
+    }
+
+    /*
+     * Private
+     * XML parser
+     */
+    private function xmlToArray(\SimpleXMLIterator $sxi)
+    {
+        $a = array();
+
+        for ($sxi->rewind(); $sxi->valid(); $sxi->next()) {
+            $t = array();
+            $current = $sxi->current();
+            $attributes = $current->attributes();
+            $name = isset($attributes->_key) ? strval($attributes->_key) : $sxi->key();
+
+            // save attributes
+            foreach ($attributes as $att_key => $att_value) {
+                if ($att_key !== '_key') {
+                    $t[$att_key] = strval($att_value);
+                }
+            }
+
+            // we parse nodes
+            if ($sxi->hasChildren()) { // children
+                $t = array_merge($t, $this->xmlToArray($current));
+            } else { // it's a leaf
+                if (empty($t)) {
+                    $t = strval($current); // strval will call _toString()
+                } else {
+                    $t['_value'] = strval($current); // strval will call _toString()
+                }
+            }
+
+            $a[$name] = $t;
+        }
+
+        return $a;
+    }
+}
---
+++ test/unit/RomaricDrigon/MetaYaml/Loader/XmlLoader.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace test\unit\RomaricDrigon\MetaYaml\Loader;
+
+use mageekguy\atoum;
+use RomaricDrigon\MetaYaml\Loader\XmlLoader as testedClass;
+
+class XmlLoader extends atoum\test
+{
+    public function testBase()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->and($xml =<<<EOT
+<fleurs>
+    <rose>une rose</rose>
+    <violette>une violette</violette>
+</fleurs>
+EOT
+            )
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\XmlLoader')
+                ->array($object->load($xml))
+                    ->isEqualTo(array(
+                        'fleurs' => array(
+                            'rose' => 'une rose',
+                            'violette' => 'une violette'
+                        )
+                    ));
+    }
+
+    public function testException()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\XmlLoader')
+                ->exception(function() use ($object) { $object->load('', true); })
+                    ->hasMessage('Error in XmlLoader : XML seems to be invalid');
+    }
+
+    public function testDeeper()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->and($xml =<<<EOT
+<fleurs>
+    <roses>
+        <opera>une rose</opera>
+        <sauvage>
+            <des_bois>une autre rose</des_bois>
+        </sauvage>
+    </roses>
+    <violette>une violette</violette>
+</fleurs>
+EOT
+        )
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\XmlLoader')
+                ->array($object->load($xml))
+                    ->isEqualTo(array(
+                        'fleurs' => array(
+                            'roses' => array(
+                                'opera' => 'une rose',
+                                'sauvage' => array (
+                                    'des_bois' => 'une autre rose'
+                                )
+                            ),
+                            'violette' => 'une violette'
+                        )
+                    ));
+    }
+
+    public function testWithKey()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->and($xml =<<<EOT
+<fleurs>
+    <roses>
+        <rose _key="opera">une rose</rose>
+        <rose _key="sauvage">une autre rose</rose>
+    </roses>
+    <violette>une violette</violette>
+</fleurs>
+EOT
+        )
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\XmlLoader')
+                ->array($object->load($xml))
+                    ->isEqualTo(array(
+                        'fleurs' => array(
+                            'roses' => array(
+                                'opera' => 'une rose',
+                                'sauvage' => 'une autre rose'
+                            ),
+                            'violette' => 'une violette'
+                        )
+                    ));
+    }
+
+    public function testAttributes()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->and($xml =<<<EOT
+<fleurs>
+    <roses couleur="rose">
+        <opera>une rose</opera>
+        <sauvage>
+            <des_bois>une autre rose</des_bois>
+        </sauvage>
+    </roses>
+    <violette couleur="violette">une violette</violette>
+</fleurs>
+EOT
+            )
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\XmlLoader')
+                ->array($object->load($xml))
+                    ->isEqualTo(array(
+                        'fleurs' => array(
+                            'roses' => array(
+                                'couleur' => 'rose',
+                                'opera' => 'une rose',
+                                'sauvage' => array (
+                                    'des_bois' => 'une autre rose'
+                                )
+                            ),
+                            'violette' => array(
+                                'couleur' => 'violette',
+                                '_value' => 'une violette'
+                            )
+                        )
+                    ));
+    }
+
+    public function testAdvancedFile()
+    {
+        $this
+            ->if($object = new testedClass())
+            ->then
+                ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\Loader\\XmlLoader')
+                ->array($object->loadFromFile('test/data/TestAdvancedXml.xml'))
+                    ->isEqualTo(array (
+                        'fleurs' => array(
+                            'roses' => array(
+                                'couleur' => 'rose',
+                                'opera' => 'une rose',
+                                'sauvage' => array(
+                                    'des_bois' => 'une autre rose',
+                                    'des_sous_bois' => array(
+                                        'sauvage' => 'oui',
+                                        '_value' => 'encore',
+                                    ),
+                                ),
+                            ),
+                            'tulipe' => 'deuxieme tulipe',
+                            'violette' => array(
+                                'couleur' => 'violette',
+                                'sauvage' => 'non',
+                                '_value' => 'une violette',
+                            ),
+                        ),
+                    ));
+    }
+}
