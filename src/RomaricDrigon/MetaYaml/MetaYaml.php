c728d6d53ea92d73c4b5780f3e2fe0c07d4e59ff
Romaric Drigon <romaric.drigon@pmsipilot.com>
2012-08-31 15:44:57 +0200

    Removed unused class MetaYaml

--- src/RomaricDrigon/MetaYaml/MetaYaml.php
+++ src/RomaricDrigon/MetaYaml/MetaYaml.php
@@ -2,44 +2,6 @@
 
 namespace RomaricDrigon\MetaYaml;
 
-use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\Definition\Builder\NodeBuilder;
-use Symfony\Component\Config\Definition\Builder\NodeDefinition;
-
 class MetaYaml
 {
-    protected $schema;
-    protected $built_schema;
-
-    /*
-     * Public functions
-     */
-
-    // function to load our schema
-    public function loadSchemaFromYaml($yaml)
-    {
-        $this->schema = Yaml::Parse($yaml);
-    }
-    
-    // validate Yaml using our schema
-    public function validateYaml($yaml)
-    {
-        return $this->validate(Yaml::Parse($yaml));
-    }
-
-    public function validate(array $data)
-    {
-        if (is_null($this->schema)) {
-            throw new \Exception('You should set schema, via loadSchema() or loadSchemaFromYaml, first !');
-        }
-
-        $validator = new SchemaValidator();
-        $validator->validate($this->schema, $data);
-
-        return true; // we could return anything!
-    }
-
 }
--- test/unit/RomaricDrigon/MetaYaml/MetaYaml.php
+++ test/unit/RomaricDrigon/MetaYaml/MetaYaml.php
@@ -10,31 +10,9 @@ class MetaYaml extends atoum\test
     public function testConstruct()
     {
         $this
-            ->if($yaml = '')
-            ->and($object = new testedClass())
+            ->if($object = new testedClass())
             ->then
                 ->object($object)->isInstanceOf('RomaricDrigon\\MetaYaml\\MetaYaml');
     }
 
-    public function testForgottenSchemaLoad()
-    {
-        $this
-            ->if($yaml = file_get_contents('test/data/TestBasicRequired.yml'))
-            ->and($object = new testedClass())
-            ->then
-                ->exception(function() use ($object, $yaml) { $object->validateYaml($yaml); })
-                ->hasMessage('You should set schema, via loadSchema() or loadSchemaFromYaml, first !');
-    }
-
-    // the basic case - ok
-    public function testBasic()
-    {
-        $this
-            ->if($schema = file_get_contents('test/data/TestBasicReference.yml'))
-            ->and($yaml = file_get_contents('test/data/TestBasicBase.yml'))
-            ->and($object = new testedClass())
-            ->and($object->loadSchemaFromYaml($schema))
-            ->then
-                ->boolean($object->validateYaml($yaml))->isEqualTo(true);
-    }
 }
